e2356bad9d4dd11a1949c50d1df9742e417cec50
tengtengcai <2373099718@qq.com>
2020-01-16 20:51:55 +0800

    进一步封装

--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -1,1 +1,11 @@
 //配置文件    此文件用来存储接口
+    // 基地址
+    // var jiDiZhi = "http://192.168.172.82:8000"
+    var jiDiZhi = "http://10.253.2.63:8000"
+    
+    // 所有接口
+    var urlList = {
+        loginUrl : jiDiZhi + "/admin/login",
+        logoutUrl : jiDiZhi + "/admin/logout",
+        userInfoUrl : jiDiZhi + "/admin/getuser"
+    }
--- web_back/js/util/request.js
+++ web_back/js/util/request.js
@@ -1,1 +1,19 @@
 //      此文件用来各种ajax请求
+
+var user = {
+    // 登陆
+    login: function (myname,mypassword) {
+        return  $.post(urlList.loginUrl, {
+            'user_name': myname,
+            'password': mypassword
+        })
+    },
+    // 注销
+    logout: function () {
+        return  $.post(urlList.logoutUrl)
+    },
+    // 获取信息
+    userInfo: function () {
+        return  $.get(urlList.userInfoUrl)  
+    }
+}
